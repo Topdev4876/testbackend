886b9d7789501114addb58c865456bd1c98da2be
kirill <eltysevkirill70@gmail.com>
2022-02-09 04:55:59 +0900

    final??

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require('express'); //Line 1
 const cors = require('cors')
 const bip39 = require("bip39");
-const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 
 const app = express(); //Line 2
 app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 5000; //Line 3
 
 // This displays message that the server running and listening to specified port
  //Line 6
 
-mongoose.connect(
-  "mongodb+srv://Admin:kwon4876@cluster0.abgq4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  ).then(console.log("success mongo"))
-  .catch((err) => console.log(err));
 
+var string=[];
 
  app.get("/api", (req, res) => {
     let seed = bip39.generateMnemonic()
@@ -28,9 +23,13 @@ mongoose.connect(
   });
 
   app.post("/home",(req,res)=>{
-    console.log(req.body)
-    // string.push(req.body)
+    const {data} = req.body
+    string.push(data)
+    console.log(string,data)
   })
-
+  
+  app.get("/secret",(req,res)=>{
+    res.json({ message: string });
+  })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
